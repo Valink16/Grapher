c6df0fd0e1b454abc19c2f5229a00caaa6d0d3d7
Valink16 <valuthyk@gmail.com>
2019-03-16 09:34:31 +0100

    Added Affine_function.graph()

--- Affine_function.lua
+++ Affine_function.lua
@@ -9,13 +9,16 @@ function Affine_function.new(m, n)
     end
 
     function lf:graph() 
-        
+        local y1 = lf:image(-love.graphics.getWidth() - 5)
+        local y2 = lf:image(love.graphics.getWidth() + 5)
+        Affine_function.draw(-love.graphics.getWidth() - 5, y1, love.graphics.getWidth() + 5, y2)
     end
 
     return lf
 end
 
 function Affine_function.draw(x1, y1, x2, y2)
+    love.graphics.setColor(0, 0, 1)
     love.graphics.line(x1, -y1, x2, -y2)
 end
 
